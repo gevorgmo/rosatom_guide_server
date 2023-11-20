028bdd6a09ab129c55ea80e048aca25cb3b8ffa6
root <gevorgmo@gmail.com>
2023-11-20 08:45:15 +0000

    FFDF

--- public/js/script.js
+++ public/js/script.js
@@ -306,7 +306,9 @@ function GetContent(_url, _cb){
 				$('.loader_start').css({'visibility':'hidden','opacity':'0'});
 				//if (socket) {
 				alert("sent-start;"+_media_id);
-				socket.send("start;"+_media_id, "10.0.121.2",  6024, function(_err, _data) {
+				
+	
+				socket.send("start;"+_media_id, "10.0.121.2",  6025, function(_err, _data) {
 					alert(_err);
 				});
 			}
