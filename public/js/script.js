74840a0aa44dd6a9ba07a7b4e6c40177049c757f
root <gevorgmo@gmail.com>
2023-11-20 08:35:19 +0000

    FFDF

--- public/js/script.js
+++ public/js/script.js
@@ -307,6 +307,7 @@ function GetContent(_url, _cb){
 				//if (socket) {
 				alert("sent-start;"+_media_id);
 				//SendUDP("start;"+_media_id, "10.0.121.2",  6024, function(data){ alert("after sent-"+data);  console.log("start;"+_media_id);});
+				var datagram = cordova.require("cordova-plugin-datagram4.datagram");
 				var socket = datagram.createSocket("multicast-udp4");
 				socket.bind(6024, function(data) {});
 				
