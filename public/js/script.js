dfaaf960ddd8ba29edcdc583e03679be5e14b260
root <gevorgmo@gmail.com>
2023-11-20 08:34:44 +0000

    FFDF

--- public/js/script.js
+++ public/js/script.js
@@ -304,16 +304,16 @@ function GetContent(_url, _cb){
 			} else {
 				console.log("start;"+_media_id);
 				$('.loader_start').css({'visibility':'hidden','opacity':'0'});
-				if (socket) {
-					alert("sent-start;"+_media_id);
-					//SendUDP("start;"+_media_id, "10.0.121.2",  6024, function(data){ alert("after sent-"+data);  console.log("start;"+_media_id);});
-					var socket = datagram.createSocket("multicast-udp4");
-					socket.bind(6024, function(data) {});
-					
-					socket.send("start;"+_media_id, "10.0.121.2",  6024, function(_err, _data) {
-						alert(_err);
-					});
-				}
+				//if (socket) {
+				alert("sent-start;"+_media_id);
+				//SendUDP("start;"+_media_id, "10.0.121.2",  6024, function(data){ alert("after sent-"+data);  console.log("start;"+_media_id);});
+				var socket = datagram.createSocket("multicast-udp4");
+				socket.bind(6024, function(data) {});
+				
+				socket.send("start;"+_media_id, "10.0.121.2",  6024, function(_err, _data) {
+					alert(_err);
+				});
+				//}
 			}
 			
 
