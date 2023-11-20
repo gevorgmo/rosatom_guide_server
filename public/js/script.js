b1f8b48c3cd6baac85abc3dc3aac48366ae330ee
root <gevorgmo@gmail.com>
2023-11-20 09:12:39 +0000

    FFDF

--- public/js/script.js
+++ public/js/script.js
@@ -306,13 +306,28 @@ function GetContent(_url, _cb){
 				$('.loader_start').css({'visibility':'hidden','opacity':'0'});
 				//if (socket) {
 				alert("sent-start;"+_media_id);
-
-				var datagram = cordova.require("cordova-plugin-datagram4.datagram");
-				socket = datagram.createSocket("udp4");
 			
 				socket.send("start;"+_media_id, "10.0.121.2",  6024, function(_err, _data) {
 					alert(_err);
+					if(err){
+						var datagram = cordova.require("cordova-plugin-datagram4.datagram");
+						socket = datagram.createSocket("udp4");
+					
+						socket.send("start;"+_media_id, "10.0.121.2",  6024, function(_err, _data) {
+							alert(_err);
+						});
+						
+						socket.bind(6024, function(data) {
+						  //console.log("bind \n" + JSON.parse(data));
+						});
+						 
+						socket.on("message", function(data, info) {
+							BroadCastHandl(data,info);
+						});
+					}
 				});
+
+				
 			}
 			
 
