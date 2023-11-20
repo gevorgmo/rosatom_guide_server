820500bbe95035fae364c7f979e4cc227de77bba
gevorgmo@gmail.com <gevorgmo@gmail.com>
2023-11-20 16:39:55 +0400

    SDFDFD

--- public/js/script.js
+++ public/js/script.js
@@ -298,50 +298,15 @@ function GetContent(_url, _cb){
 				});
 				
 				///////////////////////// OGNENSK
-				if (_media_id=="c6") {
-					socket.send("run00", "10.0.121.14",  6024, function(_err, _data) {
-						//alert(_err);
-						if(_err){
-							var datagram = cordova.require("cordova-plugin-datagram4.datagram");
-							socket = datagram.createSocket("udp4");
+				if (typeof SendUDP === "function" && _media_id=="c6")  SendUDP("run00", "10.0.121.14",  6024, function(){});
 
-							socket.send("run00", "10.0.121.14",  6024, function(_err2, _data) {	
-								socket.bind(6024, function(data) {
-								  //console.log("bind \n" + JSON.parse(data));
-								}); 
-								socket.on("message", function(data, info) {
-									BroadCastHandl(data,info);
-								});	
-								//alert(_err2);
-							});
-						}
-					});
-				}
-				
 				
 			} else {
 				console.log("start;"+_media_id);
 				$('.loader_start').css({'visibility':'hidden','opacity':'0'});
-				alert("sent-start;"+_media_id);
+				//alert("sent-start;"+_media_id);
+				if (typeof SendUDP === "function")  SendUDP("start;"+_media_id, "10.0.121.2",  6025, function(){});
 				
-				socket.send("start;"+_media_id, "10.0.121.2",  6025, function(_err, _data) {
-					alert(_err);
-					if(_err){
-						var datagram = cordova.require("cordova-plugin-datagram4.datagram");
-						socket = datagram.createSocket("udp4");
-							
-						socket.send("start;"+_media_id, "10.0.121.2",  6025, function(_err2, _data) {	
-							//socket.bind(6024, function(data) {
-							//	alert("bind "+data);
-							//}); 
-							socket.on("message", function(data, info) {
-								BroadCastHandl(data,info);
-							});
-							alert(_err2);
-						});
-
-					}
-				});	
 			}
 			
 
